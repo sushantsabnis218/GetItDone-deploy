f9cef22c3b4ef7bb9b8a1b462ca976a7a3347878
aniket <aniketmate123@gmail.com>
2021-06-18 15:36:59 +0530

    Reset Commit

--- src/main/java/com/BEProject/GetItDone/Repository/UserDao.java
+++ src/main/java/com/BEProject/GetItDone/Repository/UserDao.java
@@ -9,11 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 import com.BEProject.GetItDone.Model.User;
 
 public interface UserDao extends JpaRepository<User, Long> {
-	@Query("SELECT user FROM user_table user WHERE email = ?1")
+	@Query("SELECT * FROM User WHERE user.email = ?1")
     public User findByEmail(String email);
 	@Modifying
 	@Transactional
-	@Query(value="UPDATE user_table set name = ?2 ,email = ?3, address = ?4, contact_number= ?5 , password = ?6 where user_id = ?1", nativeQuery=true)
+	@Query(value="UPDATE User set name = ?2 ,email = ?3, address = ?4, contact_number= ?5 , password = ?6 where user_id = ?1", nativeQuery=true)
 	public void updateProfile(long user_id, String name,String email,String address,String contact_number,String password);
 }
 
