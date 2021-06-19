ba2fc3a561332b4eff33d4fda1c8a5ad5908c939
aniket <aniketmate123@gmail.com>
2021-06-19 21:16:47 +0530

    added @modelAttribute in main controller

--- src/main/java/com/BEProject/GetItDone/Controller/MainController.java
+++ src/main/java/com/BEProject/GetItDone/Controller/MainController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -56,7 +57,7 @@ public class MainController {
 	 }
 	 
 	 @PostMapping("/process_registeration")
-	 public String processRegister(@Valid UserInfo user,  Errors error, RedirectAttributes redirAttrs) {
+	 public String processRegister(@Valid @ModelAttribute("user") UserInfo user,  Errors error, RedirectAttributes redirAttrs) {
 		 if (null != error && error.getErrorCount() > 0) {
 		     return "signupForm";
 	        }
